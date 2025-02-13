6868f792da44af157c2ee9837d51edad9d0f539a
tayyabxfr <tayyabxfr@gmail.com>
2025-02-13 12:34:52 +0500

    //

--- examples/basic_sketch.cpp
+++ examples/basic_sketch.cpp
@@ -1,44 +1,44 @@
-#include <AFEVANGOV9360.h>
+// #include <AFEVANGOV9360.h>
 
-AFEVANGOV9360 AFE;
+// AFEVANGOV9360 AFE;
 
-void setup() {
-    Serial.begin(9600);
-    delay(50);
-    AFE.begin();
-      // Initialize the AFE  
-}
+// void setup() {
+//     Serial.begin(9600);
+//     delay(50);
+//     AFE.begin();
+//       // Initialize the AFE  
+// }
 
-void loop() {
+// void loop() {
 
-    float voltage = AFE.voltage() / 200087.0;
-    voltage = (voltage < 0.21) ? 0.00 : voltage;  
-    float current = (AFE.current() / 205.0) / 1000.0;
-    current = (current <= 0.02) ? 0.00 : current; 
-    int32_t power=  AFE.power();                                                                                                                                     
+//     float voltage = AFE.voltage() / 200087.0;
+//     voltage = (voltage < 0.21) ? 0.00 : voltage;  
+//     float current = (AFE.current() / 205.0) / 1000.0;
+//     current = (current <= 0.02) ? 0.00 : current; 
+//     int32_t power=  AFE.power();                                                                                                                                     
     
-    float app = voltage * current ;
+//     float app = voltage * current ;
       
-    Serial.printf("Voltage : %.2f V\n",voltage);
-    Serial.printf("Current : %.2f A\n",current);
-    if (current == 0.00)
-    {
-    power = 0.0;
-    }
-    Serial.printf("Power : %ld W\n",power);
-    float pf = abs(power)/app;
-    if (pf > 1.00 && pf < 1.02) 
-    {
-      Serial.printf("pf : 1.00\n");
-    }
-    else if(pf > 1.02)
-    {
-      Serial.printf("pf : nan\n");
-    }
-    else
-    {
-      Serial.printf("pf : %.2f\n",pf);
-    }
-    delay(1000);
-}
+//     Serial.printf("Voltage : %.2f V\n",voltage);
+//     Serial.printf("Current : %.2f A\n",current);
+//     if (current == 0.00)
+//     {
+//     power = 0.0;
+//     }
+//     Serial.printf("Power : %ld W\n",power);
+//     float pf = abs(power)/app;
+//     if (pf > 1.00 && pf < 1.02) 
+//     {
+//       Serial.printf("pf : 1.00\n");
+//     }
+//     else if(pf > 1.02)
+//     {
+//       Serial.printf("pf : nan\n");
+//     }
+//     else
+//     {
+//       Serial.printf("pf : %.2f\n",pf);
+//     }
+//     delay(1000);
+// }
   
